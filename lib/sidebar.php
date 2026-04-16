9aef641d22575587a7f60be943a171425758b791
marzo864 <daniele.marzo1234@gmail.com>
2026-04-16 13:25:26 +0200

    modifiche alla sidebar

--- lib/sidebar.php
+++ lib/sidebar.php
@@ -127,20 +127,6 @@ function classeAttiva($voce, $paginaAttiva) {
     </a>
     <?php endif; ?>
 
-    <div class="nav__section-label">Alert</div>
-
-    <a href="../home/index.php#notifiche" class="<?= classeAttiva('notifiche', $paginaAttiva) ?>" title="Notifiche">
-      <span class="nav__icon">
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-          <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9"/>
-          <path d="M13.73 21a2 2 0 01-3.46 0"/>
-        </svg>
-      </span>
-      <span class="nav__label">Notifiche</span>
-      <?php if ($hasFire): ?>
-      <span class="nav__badge">!</span>
-      <?php endif; ?>
-    </a>
 
   </nav>
 
